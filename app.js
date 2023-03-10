f4d0221461cb35244a493a3383853594e570b1ca
Jacobo-Restrepo <123768195+Jacobo-Restrepo@users.noreply.github.com>
2023-03-10 13:55:09 -0500

    cambio8

--- app.js
+++ app.js
@@ -31,15 +31,23 @@ document.addEventListener("DOMContentLoaded", () => {
   var cartasEscogidasId = [];
   var cartasGanadas = [];
 
-function crearTablero() {
-
-for (let i = 0; i < cardAdj.length; i++) { 
-  var carta = document.createElement("img");
-  carta.setAttribute("src", "images/reverso.png");
-  carta.setAttribute("data-id", i);
-  carta.addEventListener("click", voltearCarta);
-  cuadricula.appendChild(carta);
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+      cuadricula.appendChild(carta);
+    }
   }
-}
-
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
+  }
+  crearTablero();
 });
